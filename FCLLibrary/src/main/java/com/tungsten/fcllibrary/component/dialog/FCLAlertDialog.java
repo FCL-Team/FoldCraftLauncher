7e656501038f59b992caba4b84ff6f6233613436
Tungstend <hanjinlong197@gmail.com>
2022-10-26 22:31:38 +0800

    fix color picker dialog layout & add alert dialog

--- FCLLibrary/src/main/java/com/tungsten/fcllibrary/component/dialog/FCLAlertDialog.java
+++ FCLLibrary/src/main/java/com/tungsten/fcllibrary/component/dialog/FCLAlertDialog.java
@@ -1,13 +1,167 @@
 package com.tungsten.fcllibrary.component.dialog;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.view.View;
 
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.utils.widget.ImageFilterView;
 
-public class FCLAlertDialog extends FCLDialog {
+import com.tungsten.fcllibrary.R;
+import com.tungsten.fcllibrary.component.view.FCLButton;
+import com.tungsten.fcllibrary.component.view.FCLTextView;
 
+public class FCLAlertDialog extends FCLDialog implements View.OnClickListener {
+
+    private String titleString;
+
+    private ButtonListener positiveListener;
+    private ButtonListener negativeListener;
+
+    private ImageFilterView icon;
+    private FCLTextView title;
+    private FCLTextView message;
+    private FCLButton positive;
+    private FCLButton negative;
+
+    @SuppressLint("UseCompatLoadingForDrawables")
     public FCLAlertDialog(@NonNull Context context) {
         super(context);
+        setContentView(R.layout.dialog_alert);
+
+        icon = findViewById(R.id.image);
+        title = findViewById(R.id.title);
+        message = findViewById(R.id.text);
+        positive = findViewById(R.id.positive);
+        negative = findViewById(R.id.negative);
+
+        positive.setVisibility(View.GONE);
+        negative.setVisibility(View.GONE);
+
+        positive.setOnClickListener(this);
+        negative.setOnClickListener(this);
+
+        icon.setImageDrawable(getContext().getDrawable(R.drawable.ic_baseline_info_24));
+        title.setText(getContext().getString(R.string.dialog_info));
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == positive) {
+            if (positiveListener != null) {
+                positiveListener.onClick();
+            }
+            dismiss();
+        }
+        if (view == negative) {
+            if (negativeListener != null) {
+                negativeListener.onClick();
+            }
+            dismiss();
+        }
+    }
+
+    @SuppressLint("UseCompatLoadingForDrawables")
+    public void setAlertLevel(AlertLevel alertLevel) {
+        switch (alertLevel) {
+            case ALERT:
+                icon.setImageDrawable(getContext().getDrawable(R.drawable.ic_baseline_warning_24));
+                if (titleString == null) {
+                    title.setText(getContext().getString(R.string.dialog_alert));
+                }
+                break;
+            default:
+                icon.setImageDrawable(getContext().getDrawable(R.drawable.ic_baseline_info_24));
+                if (titleString == null) {
+                    title.setText(getContext().getString(R.string.dialog_info));
+                }
+                break;
+        }
+    }
+
+    public void setTitle(String title) {
+        titleString = title;
+        this.title.setText(title);
+    }
+
+    public void setMessage(String message) {
+        this.message.setText(message);
+    }
+
+    public void setPositiveButton(String text, ButtonListener listener) {
+        positive.setVisibility(View.VISIBLE);
+        positive.setText(text);
+        positiveListener = listener;
+    }
+
+    public void setNegativeButton(String text, ButtonListener listener) {
+        negative.setVisibility(View.VISIBLE);
+        negative.setText(text);
+        negativeListener = listener;
+    }
+
+    public static class Builder {
+
+        private Context context;
+        private FCLAlertDialog dialog;
+
+        public Builder(Context context) {
+            this.context = context;
+            dialog = new FCLAlertDialog(context);
+        }
+
+        public FCLAlertDialog create() {
+            return dialog;
+        }
+
+        public Builder setAlertLevel(AlertLevel alertLevel) {
+            dialog.setAlertLevel(alertLevel);
+            return this;
+        }
+
+        public Builder setCancelable(boolean cancelable) {
+            dialog.setCancelable(cancelable);
+            return this;
+        }
+
+        public Builder setTitle(String title) {
+            dialog.setTitle(title);
+            return this;
+        }
+
+        public Builder setMessage(String message) {
+            dialog.setMessage(message);
+            return this;
+        }
+
+        public Builder setPositiveButton(ButtonListener listener) {
+            dialog.setPositiveButton(context.getString(R.string.dialog_positive), listener);
+            return this;
+        }
+
+        public Builder setPositiveButton(String text, ButtonListener listener) {
+            dialog.setPositiveButton(text, listener);
+            return this;
+        }
+
+        public Builder setNegativeButton(ButtonListener listener) {
+            dialog.setNegativeButton(context.getString(R.string.dialog_negative), listener);
+            return this;
+        }
+
+        public Builder setNegativeButton(String text, ButtonListener listener) {
+            dialog.setNegativeButton(text, listener);
+            return this;
+        }
+    }
+
+    public enum AlertLevel {
+        ALERT,
+        INFO
+    }
+
+    public interface ButtonListener {
+        void onClick();
     }
 
 }
