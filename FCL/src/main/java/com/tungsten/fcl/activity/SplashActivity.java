4ce0f7e89b61337d98d24661a06447fc0fd2ec5c
Tungstend <hanjinlong197@gmail.com>
2022-10-29 00:56:52 +0800

    create Chinese translation

--- FCL/src/main/java/com/tungsten/fcl/activity/SplashActivity.java
+++ FCL/src/main/java/com/tungsten/fcl/activity/SplashActivity.java
@@ -1,4 +1,19 @@
 package com.tungsten.fcl.activity;
 
-public class SplashActivity {
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+import com.tungsten.fcl.R;
+import com.tungsten.fcllibrary.component.FCLActivity;
+
+@SuppressLint("CustomSplashScreen")
+public class SplashActivity extends FCLActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
 }
