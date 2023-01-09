794e315c4e6be37742295a250389a20f7be72969
Tungstend <hanjinlong197@gmail.com>
2023-01-09 23:58:27 +0800

    add touch pad

---
+++ FCL/src/main/java/com/tungsten/fcl/control/view/TouchPad.java
@@ -0,0 +1,23 @@
+package com.tungsten.fcl.control.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
+public class TouchPad extends RelativeLayout {
+    public TouchPad(Context context) {
+        super(context);
+    }
+
+    public TouchPad(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public TouchPad(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public TouchPad(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+}
