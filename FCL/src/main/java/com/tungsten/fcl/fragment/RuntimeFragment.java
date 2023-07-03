b2c823d9f883cda5230c29a40827830fde33ff82
Tungstend <hanjinlong197@gmail.com>
2023-07-03 18:10:55 +0800

    update splash activity ui

--- FCL/src/main/java/com/tungsten/fcl/fragment/RuntimeFragment.java
+++ FCL/src/main/java/com/tungsten/fcl/fragment/RuntimeFragment.java
@@ -144,7 +144,13 @@ public class RuntimeFragment extends FCLFragment implements View.OnClickListener
         }
     }
 
+    private boolean installing = false;
+
     private void install() {
+        if (installing)
+            return;
+
+        installing = true;
         if (!lwjgl2) {
             lwjgl2State.setVisibility(View.GONE);
             lwjgl2Progress.setVisibility(View.VISIBLE);
