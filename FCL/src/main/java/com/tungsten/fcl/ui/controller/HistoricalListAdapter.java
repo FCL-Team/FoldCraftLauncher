ff180c20acf2dfd0bd12e5e30e70a3e50e9c64c7
Tungstend <hanjinlong197@gmail.com>
2024-09-22 15:22:14 +0800

    Feat[Controller]: download controller

---
+++ FCL/src/main/java/com/tungsten/fcl/ui/controller/HistoricalListAdapter.java
@@ -0,0 +1,73 @@
+package com.tungsten.fcl.ui.controller;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.tungsten.fcl.R;
+import com.tungsten.fcl.control.download.ControllerVersion;
+import com.tungsten.fcllibrary.component.FCLAdapter;
+import com.tungsten.fcllibrary.component.theme.ThemeEngine;
+import com.tungsten.fcllibrary.component.view.FCLLinearLayout;
+import com.tungsten.fcllibrary.component.view.FCLTextView;
+import com.tungsten.fcllibrary.util.ConvertUtils;
+
+import java.util.ArrayList;
+
+public class HistoricalListAdapter extends FCLAdapter {
+
+    private ArrayList<ControllerVersion.VersionInfo> list;
+    private Callback callback;
+
+    public HistoricalListAdapter(Context context, ArrayList<ControllerVersion.VersionInfo> list, Callback callback) {
+        super(context);
+        this.list = list;
+        this.callback = callback;
+    }
+
+    private static class ViewHolder {
+        FCLLinearLayout parent;
+        FCLTextView version;
+    }
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return list.get(i);
+    }
+
+    @SuppressLint({"UseCompatLoadingForDrawables", "SetTextI18n"})
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        final ViewHolder viewHolder;
+        if (view == null) {
+            viewHolder = new ViewHolder();
+            view = new FCLLinearLayout(getContext());
+            viewHolder.parent = new FCLLinearLayout(getContext());
+            viewHolder.version = new FCLTextView(getContext());
+            ((FCLLinearLayout) view).addView(viewHolder.parent, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+            int padding = ConvertUtils.dip2px(getContext(), 10);
+            viewHolder.parent.setPadding(padding, padding, padding, padding);
+            viewHolder.parent.setBackground(getContext().getDrawable(R.drawable.bg_container_transparent_clickable));
+            viewHolder.parent.addView(viewHolder.version, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+            viewHolder.version.setSingleLine(true);
+            viewHolder.version.setAutoTint(true);
+            viewHolder.version.setTextColor(ThemeEngine.getInstance().getTheme().getAutoTint());
+            view.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) view.getTag();
+        }
+        viewHolder.parent.setOnClickListener(v -> callback.onSelect(list.get(i)));
+        viewHolder.version.setText(list.get(i).getVersionName());
+        return view;
+    }
+
+    public interface Callback {
+        void onSelect(ControllerVersion.VersionInfo versionInfo);
+    }
+}
