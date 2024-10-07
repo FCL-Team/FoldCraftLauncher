a78dadf7f924c9c19c819af5affc180b9fbddfbc
ShirosakiMio <852468399@qq.com>
2024-10-07 14:09:26 +0800

    Feat[Mod]: add "MC百科"

--- FCL/src/main/java/com/tungsten/fcl/ui/download/RemoteModInfoPage.java
+++ FCL/src/main/java/com/tungsten/fcl/ui/download/RemoteModInfoPage.java
@@ -2,8 +2,7 @@ package com.tungsten.fcl.ui.download;
 
 import android.content.Context;
 import android.content.res.ColorStateList;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+import android.util.Log;
 import android.view.View;
 import android.widget.ListView;
 
@@ -20,22 +19,18 @@ import com.tungsten.fclcore.task.Task;
 import com.tungsten.fclcore.util.SimpleMultimap;
 import com.tungsten.fclcore.util.StringUtils;
 import com.tungsten.fclcore.util.versioning.VersionNumber;
-import com.tungsten.fcllibrary.component.view.FCLLinearLayout;
-import com.tungsten.fcllibrary.util.LocaleUtils;
 import com.tungsten.fcllibrary.component.theme.ThemeEngine;
 import com.tungsten.fcllibrary.component.ui.FCLTempPage;
 import com.tungsten.fcllibrary.component.view.FCLImageButton;
 import com.tungsten.fcllibrary.component.view.FCLImageView;
+import com.tungsten.fcllibrary.component.view.FCLLinearLayout;
 import com.tungsten.fcllibrary.component.view.FCLProgressBar;
 import com.tungsten.fcllibrary.component.view.FCLTextView;
 import com.tungsten.fcllibrary.component.view.FCLUILayout;
+import com.tungsten.fcllibrary.util.LocaleUtils;
 
 import org.jetbrains.annotations.Nullable;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -63,6 +58,7 @@ public class RemoteModInfoPage extends FCLTempPage implements View.OnClickListen
     private FCLTextView name;
     private FCLTextView tag;
     private FCLTextView description;
+    private FCLTextView mcmod;
     private FCLImageButton website;
 
     public RemoteModInfoPage(Context context, int id, FCLUILayout parent, int resId, DownloadPage page, RemoteMod addon, Profile.ProfileVersion version, @Nullable RemoteModVersionPage.DownloadCallback callback) {
@@ -88,12 +84,14 @@ public class RemoteModInfoPage extends FCLTempPage implements View.OnClickListen
         name = findViewById(R.id.name);
         tag = findViewById(R.id.tag);
         description = findViewById(R.id.description);
+        mcmod = findViewById(R.id.mcmod);
         website = findViewById(R.id.website);
 
         retry.setOnClickListener(this);
+        mcmod.setOnClickListener(this);
         website.setOnClickListener(this);
 
-        ThemeEngine.getInstance().registerEvent(versionListView, () -> versionListView.setBackgroundTintList(new ColorStateList(new int[][] { { } }, new int[] { ThemeEngine.getInstance().getTheme().getLtColor() })));
+        ThemeEngine.getInstance().registerEvent(versionListView, () -> versionListView.setBackgroundTintList(new ColorStateList(new int[][]{{}}, new int[]{ThemeEngine.getInstance().getTheme().getLtColor()})));
     }
 
     @Override
@@ -103,6 +101,7 @@ public class RemoteModInfoPage extends FCLTempPage implements View.OnClickListen
         icon.setImageDrawable(null);
         Glide.with(getContext()).load(addon.getIconUrl()).into(icon);
         ModTranslations.Mod mod = translations.getModByCurseForgeId(addon.getSlug());
+        mcmod.setVisibility(mod == null ? View.GONE : View.VISIBLE);
         name.setText(mod != null && LocaleUtils.isChinese(getContext()) ? mod.getDisplayName() : addon.getTitle());
         description.setText(addon.getDescription());
         List<String> categories = addon.getCategories().stream().map(page::getLocalizedCategory).collect(Collectors.toList());
@@ -189,6 +188,14 @@ public class RemoteModInfoPage extends FCLTempPage implements View.OnClickListen
     public void onClick(View v) {
         if (v == retry) {
             loadModVersions();
+        }
+        if (v == mcmod) {
+            ModTranslations.Mod mod = translations.getModByCurseForgeId(addon.getSlug());
+            if (mod != null) {
+                String url = translations.getMcmodUrl(mod);
+                Log.e("测试", url);
+                AndroidUtils.openLink(getContext(), url);
+            }
         }
         if (v == website && StringUtils.isNotBlank(addon.getPageUrl())) {
             AndroidUtils.openLink(getContext(), addon.getPageUrl());
