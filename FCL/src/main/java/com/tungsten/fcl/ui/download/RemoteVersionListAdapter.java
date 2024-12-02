179a660a5f2e49179a4710518267ebc8c40ccda6
ShirosakiMio <852468399@qq.com>
2024-12-02 12:18:57 +0800

    Update RemoteVersionListAdapter.java

--- FCL/src/main/java/com/tungsten/fcl/ui/download/RemoteVersionListAdapter.java
+++ FCL/src/main/java/com/tungsten/fcl/ui/download/RemoteVersionListAdapter.java
@@ -89,7 +89,7 @@ public class RemoteVersionListAdapter extends FCLAdapter {
         viewHolder.tag.setText(getTag(remoteVersion));
         viewHolder.date.setVisibility(remoteVersion.getReleaseDate() == null ? View.GONE : View.VISIBLE);
         viewHolder.date.setText(remoteVersion.getReleaseDate() == null ? "" : formatDateTime(getContext(), remoteVersion.getReleaseDate()));
-        if (remoteVersion.getVersionType() == RemoteVersion.Type.RELEASE || remoteVersion.getVersionType() == RemoteVersion.Type.SNAPSHOT) {
+        if (remoteVersion instanceof GameRemoteVersion && (remoteVersion.getVersionType() == RemoteVersion.Type.RELEASE || remoteVersion.getVersionType() == RemoteVersion.Type.SNAPSHOT)) {
             viewHolder.wiki.setVisibility(View.VISIBLE);
             viewHolder.wiki.setOnClickListener(v -> AndroidUtils.openLink(getContext(), remoteVersion.getVersionType() == RemoteVersion.Type.RELEASE ? String.format(getContext().getString(R.string.wiki_release), remoteVersion.getGameVersion()) : String.format(getContext().getString(R.string.wiki_snapshot), remoteVersion.getGameVersion())));
         } else {
