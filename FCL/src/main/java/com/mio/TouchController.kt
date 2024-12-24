f3acd82895f0d16b328dde17c2f375229ded8a9c
ShirosakiMio <852468399@qq.com>
2024-12-24 15:24:57 +0800

    Update TouchController.kt

--- FCL/src/main/java/com/mio/TouchController.kt
+++ FCL/src/main/java/com/mio/TouchController.kt
@@ -6,10 +6,12 @@ import android.system.Os
 import android.util.Log
 import android.util.SparseIntArray
 import android.view.MotionEvent
+import com.tungsten.fclcore.util.Logging
 import top.fifthlight.touchcontroller.proxy.client.LauncherProxyClient
 import top.fifthlight.touchcontroller.proxy.client.android.SimpleVibrationHandler
 import top.fifthlight.touchcontroller.proxy.client.android.transport.UnixSocketTransport
 import top.fifthlight.touchcontroller.proxy.data.Offset
+import java.util.logging.Level
 
 class TouchController(context: Context, val width: Int, val height: Int) {
     private var client: LauncherProxyClient? = null
@@ -31,7 +33,11 @@ class TouchController(context: Context, val width: Int, val height: Int) {
             client?.vibrationHandler = handler
             client?.run()
         } catch (ex: Throwable) {
-            Log.w("TouchController", "TouchController proxy client create failed", ex)
+            Logging.LOG.log(
+                Level.WARNING,
+                "TouchController: TouchController proxy client create failed",
+                ex
+            );
         }
     }
 
