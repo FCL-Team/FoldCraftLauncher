7746a9432ecf8b2828c94e107ee6619e54965015
Tungstend <74163103+Tungstend@users.noreply.github.com>
2025-01-12 11:33:03 +0800

    Update announcement link

--- FCL/src/main/java/com/tungsten/fcl/ui/main/MainUI.java
+++ FCL/src/main/java/com/tungsten/fcl/ui/main/MainUI.java
@@ -32,7 +32,7 @@ import java.util.logging.Level;
 
 public class MainUI extends FCLCommonUI implements View.OnClickListener {
 
-    public static final String ANNOUNCEMENT_URL = "https://gitcode.net/fcl-team/fold-craft-launcher/-/raw/master/res/announcement_v2.txt?inline=false";
+    public static final String ANNOUNCEMENT_URL = "https://raw.gitcode.com/fcl-team1/fold-craft-launcher/raw/master/res/announcement_v2.txt";
 
     private LinearLayoutCompat announcementContainer;
     private LinearLayoutCompat announcementLayout;
