81ec439024fb31c39e0e36dfc53ff0819e24ab99
ShirosakiMio <852468399@qq.com>
2025-03-02 18:47:44 +0800

    Update to Java 11

--- FCLauncher/build.gradle.kts
+++ FCLauncher/build.gradle.kts
@@ -25,8 +25,8 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
 
     externalNativeBuild {
@@ -42,7 +42,7 @@ android {
     }
 
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "11"
     }
 }
 
