878ecf91f9c1b31c350e53c0e4590e343b5c08b5
ShirosakiMio <852468399@qq.com>
2025-03-05 09:32:07 +0800

    Fix: Unable to send combination keys (#886)
    * Update ControlButton.java
    
    * Update ControlButton.java
    
    * Update ControlButton.java

--- FCL/src/main/java/com/tungsten/fcl/control/view/ControlButton.java
+++ FCL/src/main/java/com/tungsten/fcl/control/view/ControlButton.java
@@ -277,6 +277,7 @@ public class ControlButton extends AppCompatButton implements CustomView {
 
     private final Handler handler = new Handler();
     private final Runnable runnable = () -> handleLongPressEvent(!longPressEvent);
+
     private void deleteView() {
         if (menu != null) {
             menu.getViewManager().removeView(getData());
@@ -573,16 +574,16 @@ public class ControlButton extends AppCompatButton implements CustomView {
         if (!press && !keycodeOutputting) {
             return;
         }
-        Schedulers.io().execute(()->{
+        new Thread(() -> {
             for (int keycode : event.outputKeycodesList()) {
                 keycodeOutputting = press;
                 menu.getInput().sendKeyEvent(keycode, press);
                 try {
-                    Thread.sleep(100);
+                    Thread.sleep(5);
                 } catch (InterruptedException ignore) {
                 }
             }
-        });
+        }).start();
     }
 
     private boolean autoClick = false;
