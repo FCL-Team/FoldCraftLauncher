4230207b0b98b75a649d460592b5b43b893e579a
ShirosakiMio <852468399@qq.com>
2025-03-10 09:48:39 +0800

    Update

---
+++ FCL/src/main/java/com/mio/ui/dialog/JavaManagerDialog.kt
@@ -0,0 +1,10 @@
+package com.mio.ui.dialog
+
+import android.content.Context
+import com.tungsten.fcllibrary.component.dialog.FCLDialog
+
+class JavaManagerDialog(context: Context): FCLDialog(context) {
+    init {
+
+    }
+}
