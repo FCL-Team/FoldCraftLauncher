275c7d630d3113f4463c49da4daf70c866f92bf4
ShirosakiMio <852468399@qq.com>
2025-03-21 15:23:26 +0800

    Remove com.jaredrummler:android-device-names

--- FCLauncher/src/main/java/com/tungsten/fclauncher/plugins/FFmpegPlugin.java
+++ FCLauncher/src/main/java/com/tungsten/fclauncher/plugins/FFmpegPlugin.java
@@ -14,8 +14,7 @@ public class FFmpegPlugin {
             PackageInfo ffmpegPluginInfo = manager.getPackageInfo("net.kdt.pojavlaunch.ffmpeg", PackageManager.GET_SHARED_LIBRARY_FILES);
             libraryPath = ffmpegPluginInfo.applicationInfo.nativeLibraryDir;
             isAvailable = true;
-        }catch (Exception e) {
-            Log.i("FFmpegPlugin", "Failed to discover plugin", e);
+        }catch (Exception ignore) {
         }
     }
 }
