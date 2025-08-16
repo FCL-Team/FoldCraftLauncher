400750782bffdd04e9679e2fc5cd9ac5e0fd12f5
ShirosakiMio <852468399@qq.com>
2025-08-16 10:04:07 +0800

    暂时禁用 MCIM 加速
    https://github.com/HMCL-dev/HMCL/commit/f6a3b76a0b558fcbf9a88d45b42b193eb40ce081

--- FCLCore/src/main/java/com/tungsten/fclcore/download/BMCLAPIDownloadProvider.java
+++ FCLCore/src/main/java/com/tungsten/fclcore/download/BMCLAPIDownloadProvider.java
@@ -77,11 +77,11 @@ public final class BMCLAPIDownloadProvider implements DownloadProvider {
                 pair("https://repo1.maven.org/maven2", "https://mirrors.cloud.tencent.com/nexus/repository/maven-public"),
 
                 // https://github.com/mcmod-info-mirror/mcim-rust-api
-                pair("https://api.modrinth.com", "https://mod.mcimirror.top/modrinth"),
-                pair("https://cdn.modrinth.com", "https://mod.mcimirror.top"),
-                pair("https://api.curseforge.com", "https://mod.mcimirror.top/curseforge"),
-                pair("https://edge.forgecdn.net", "https://mod.mcimirror.top"),
-                pair("https://mediafilez.forgecdn.net", "https://mod.mcimirror.top")
+//                pair("https://api.modrinth.com", "https://mod.mcimirror.top/modrinth"),
+//                pair("https://cdn.modrinth.com", "https://mod.mcimirror.top"),
+//                pair("https://api.curseforge.com", "https://mod.mcimirror.top/curseforge"),
+//                pair("https://edge.forgecdn.net", "https://mod.mcimirror.top"),
+//                pair("https://mediafilez.forgecdn.net", "https://mod.mcimirror.top")
         );
     }
 
