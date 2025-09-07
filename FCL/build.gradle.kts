892fbde12e2b213db9fad5bd8e7c2ce6d7622a0c
ShirosakiMio <852468399@qq.com>
2025-09-07 09:43:56 +0800

    1.2.5.1

--- FCL/build.gradle.kts
+++ FCL/build.gradle.kts
@@ -41,8 +41,8 @@ android {
         applicationId = "com.tungsten.fcl"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 1250
-        versionName = "1.2.5.0"
+        versionCode = 1251
+        versionName = "1.2.5.1"
     }
 
     buildTypes {
