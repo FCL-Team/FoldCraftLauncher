3b060106a7e9a2b46b7028af5da42e9759477187
ShirosakiMio <852468399@qq.com>
2025-10-07 13:05:22 +0800

    Update SplashActivity.kt

--- FCL/src/main/java/com/tungsten/fcl/activity/SplashActivity.kt
+++ FCL/src/main/java/com/tungsten/fcl/activity/SplashActivity.kt
@@ -26,14 +26,11 @@ import com.tungsten.fcl.fragment.EulaFragment
 import com.tungsten.fcl.fragment.RuntimeFragment
 import com.tungsten.fcl.setting.ConfigHolder
 import com.tungsten.fcl.util.RuntimeUtils
-import com.tungsten.fclauncher.plugins.DriverPlugin
-import com.tungsten.fclauncher.plugins.RendererPlugin
 import com.tungsten.fclauncher.utils.FCLPath
 import com.tungsten.fclcore.util.Logging
 import com.tungsten.fclcore.util.io.FileUtils
 import com.tungsten.fcllibrary.component.FCLActivity
 import com.tungsten.fcllibrary.component.dialog.FCLAlertDialog
-import com.tungsten.fcllibrary.component.dialog.FCLAlertDialog.ButtonListener
 import com.tungsten.fcllibrary.component.theme.ThemeEngine
 import com.tungsten.fcllibrary.util.LocaleUtils
 import kotlinx.coroutines.Dispatchers
