29a06b2fdc632cca1e6823c75a2caf13aabb058d
ShirosakiMio <852468399@qq.com>
2026-04-12 18:36:21 +0800

    feat(LWJGL): 添加3.4.1

--- LWJGL/build.gradle.kts
+++ LWJGL/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     java
 }
 
-val lwjglVersions = arrayOf("3.3.3"/*, "3.4.1"*/)
+val lwjglVersions = arrayOf("3.3.3", "3.4.1")
 
 tasks.register("buildLwjgl") {
     dependsOn(lwjglVersions.map { ":LWJGL:$it:jar" })
