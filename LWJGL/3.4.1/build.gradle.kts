01ecbab26640bce0e9e2c43ae094418f13e3f70c
ShirosakiMio <852468399@qq.com>
2026-04-12 18:36:21 +0800

    feat(LWJGL): 添加3.4.1

---
+++ LWJGL/3.4.1/build.gradle.kts
@@ -0,0 +1,93 @@
+import java.io.File
+
+plugins {
+    java
+}
+
+val lwjglVersion = "3.4.1"
+group = "org.lwjgl.glfw"
+
+configurations {
+    create("lwjglModules") {
+        isCanBeResolved = true
+    }
+}
+
+dependencies {
+    compileOnly(fileTree(mapOf("dir" to "../compileOnly", "include" to listOf("*.jar"))))
+    implementation(fileTree(mapOf("dir" to "libs/$lwjglVersion", "include" to listOf("*.jar"))))
+    add("lwjglModules", fileTree(mapOf("dir" to "libs/$lwjglVersion", "include" to listOf("*.jar"))))
+    implementation(libs.jspecify) // lwjgl3.3.3 has jsr305 included as a jar
+
+}
+
+tasks.jar {
+    // Modules to copy over to the components directory instead of patching and merging
+    val excludedModules = arrayOf(
+        "jsr305.jar",
+        "lwjgl.jar",
+        "lwjgl-freetype.jar",
+        "lwjgl-jemalloc.jar",
+        "lwjgl-lwjglx.jar",
+        "lwjgl-nanovg.jar",
+        "lwjgl-openal.jar",
+        "lwjgl-opengl.jar",
+        "lwjgl-stb.jar",
+        "lwjgl-tinyfd.jar",
+    )
+
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    archiveBaseName.set("lwjgl-${lwjglVersion}-merged-modules")
+    destinationDirectory.set(file("$rootDir/FCL/src/main/assets/app_runtime/lwjgl/${lwjglVersion}"))
+
+    // Mark as using multiple Java versions.
+    manifest {
+        attributes("Multi-Release" to "true")
+        attributes("Automatic-Module-Name" to "org.lwjgl")
+    }
+
+    from({
+        // Ensure that the core lwjgl jar is processed first so duplicates in META-INF from other classes
+        // are ignored. This avoids InvalidModuleDescriptorException due to say, using the module-info.class
+        // from lwjgl-jemalloc.
+        val includedModules = configurations["lwjglModules"].filter { dep ->
+            !excludedModules.any { it == dep.name }
+        }
+        val coreJar = includedModules.find { it.name == "lwjgl.jar" }
+        val jarList = if (coreJar != null) listOf(coreJar) + (includedModules - coreJar) else includedModules
+        println("Merging LWJGL $lwjglVersion modules in the order: ")
+        jarList.map {
+            println(it.name)
+            if (it.isDirectory) it else zipTree(it)
+        }
+    })
+
+    // Makes the jar reproducible so the version file actually is a version file
+    isPreserveFileTimestamps = false
+    isReproducibleFileOrder = true
+
+    val versionFile = File(destinationDirectory.get().asFile, "version")
+    doLast {
+        val excludedModulesFileList = excludedModules.flatMap { fileName ->
+            configurations["lwjglModules"].filter { it.name == fileName }
+        }
+        copy {
+            // Copy excluded modules to the lwjgl classes dir
+            from(excludedModulesFileList)
+            into(archiveFile.get().asFile.parentFile)
+        }
+        versionFile.writeText(System.currentTimeMillis().toString())
+    }
+    // Adds the jank to outputs
+    outputs.file(versionFile)
+    outputs.files(excludedModules.map { path -> File(destinationDirectory.get().asFile, path) })
+    exclude("net/java/openjdk/cacio/ctc/**")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
