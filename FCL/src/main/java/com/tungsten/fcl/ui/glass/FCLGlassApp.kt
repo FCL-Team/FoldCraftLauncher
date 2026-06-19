704e446fed164be45d6848d066ee7f49b5eee288
xiaowuzt <xiaowuzt@users.noreply.github.com>
2026-06-19 12:04:37 +0000

    chore(glass): clean unused imports

--- FCL/src/main/java/com/tungsten/fcl/ui/glass/FCLGlassApp.kt
+++ FCL/src/main/java/com/tungsten/fcl/ui/glass/FCLGlassApp.kt
@@ -16,7 +16,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.currentBackStackEntryAsState
