9ed6bcf313dbc503049b68788ed90c402a7bd952
ShirosakiMio <852468399@qq.com>
2026-09-13 09:58:25 +0800

    1.3.3.2

--- FCL/build.gradle.kts
+++ FCL/build.gradle.kts
@@ -69,8 +69,8 @@ android {
         applicationId = "com.tungsten.fcl"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 1331
-        versionName = "1.3.3.1"
+        versionCode = 1332
+        versionName = "1.3.3.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         externalNativeBuild {
             cmake {
