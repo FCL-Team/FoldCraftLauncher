d03b6075c16ef3dcb1b95c42a8fdcbef93546e74
ShirosakiMio <852468399@qq.com>
2026-09-15 08:34:15 +0800

    optimize: DialogCard 条目间距收紧到水平 3dp、垂直 2dp

--- FCL/src/main/java/com/mio/skin/AnimationDialog.kt
+++ FCL/src/main/java/com/mio/skin/AnimationDialog.kt
@@ -56,8 +56,8 @@ class AnimationDialog(
             ViewGroup.LayoutParams.WRAP_CONTENT
         )
         params.topMargin = (6 * density).toInt()
-        params.marginStart = (6 * density).toInt()
-        params.marginEnd = (6 * density).toInt()
+        params.marginStart = (3 * density).toInt()
+        params.marginEnd = (3 * density).toInt()
         return params
     }
 
